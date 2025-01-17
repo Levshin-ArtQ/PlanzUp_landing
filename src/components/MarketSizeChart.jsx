d30826f98e25482589bc51d4118e9cc7e08375e4
Levshin-ArtQ <114755543+Levshin-ArtQ@users.noreply.github.com>
2025-01-17 15:38:53 +0500

    for github commit

---
+++ src/components/MarketSizeChart.jsx
@@ -0,0 +1,42 @@
+// import React from 'react';
+import { Doughnut } from 'react-chartjs-2';
+// import { useTranslation } from 'react-i18next';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, ArcElement, Title, Tooltip, Legend } from 'chart.js';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, ArcElement, Title, Tooltip, Legend);
+
+const data = {
+  labels: ['TAM', 'SAM', 'SOM'],
+  datasets: [
+    {
+      label: 'Market Size in $M',
+      data: [500, 200, 100],
+      backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+      hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
+    }
+  ]
+};
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top'
+    },
+    title: {
+      display: true,
+      text: 'Market Size'
+    }
+  }
+};
+
+
+
+const MarketSizeChart = (title) => (
+  <div>
+    <h2 className="text-center">{title.title} 🌐</h2>
+    <Doughnut data={data} options={options} />
+  </div>
+);
+
+export default MarketSizeChart;
